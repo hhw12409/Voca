2029674d85837a9f7cb48e3171271b4e7fbc1cd5
hhw12409 <sksk2425@naver.com>
2022-06-28 00:59:14 +0900

    Docs : change typescript

---
+++ src/components/DayList.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+
+export interface IDay {
+  id: number;
+  day: number;
+}
+
+export default function DayList() {
+  
+  const days: IDay[] = useFetch("http://localhost:3001/days")
+
+  if(days.length === -1) {
+    return ( 
+      <div>Loding...</div>
+    )
+  }
+
+  return (
+    <>
+      <ul className="list_day">
+        {
+          days.map(day => (
+            <li key={day.id}>
+              <Link to={`/day/${day.id}`}>Day {day.day}</Link>
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
+}
