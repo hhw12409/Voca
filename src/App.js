56f7870d39f03311294125e91c0471c4a36166ca
hhw12409 <sksk2425@naver.com>
2022-06-28 00:57:53 +0900

    Feat : edit typescript

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import DayList from './components/DayList';
 import Day from './components/Day';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import EmptyPage from './components/EmptyPage';
+import NewWord from './components/NewWord';
+import NewDay from './components/NewDay';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={ <DayList/> }/>
         <Route path="/day/:day" element={ <Day/> }/>
+        <Route path="/new-word" element={ <NewWord/> }/>
+        <Route path="/new-day" element={ <NewDay/> }/>
         <Route path="*" element={ <EmptyPage/> }/>
       </Routes>
     </div>
